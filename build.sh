ce3cfcb7770d984c881482a9c27fa4ae73048474
Clément <clementfiesta@gmail.com>
2025-07-14 14:54:14 +0200

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "@nearlyapp:registry=https://npm.pkg.github.com
+//npm.pkg.github.com/:_authToken=$NODE_AUTH_TOKEN
+always-auth=true" > .npmrc
+
+npm install --include=dev
+
+npm run build
+
+npm run drizzle:migrate
