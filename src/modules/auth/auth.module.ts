26fc2a1b2c5e4042e919b412a045578bb7b94a23
Clément <clementfiesta@gmail.com>
2025-07-17 12:42:54 +0200

    Small tweaks

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,6 +1,7 @@
-import { AuthController } from '@modules/auth/auth.controller';
-import { SessionSerializer } from '@modules/auth/session.serializer';
-import { LocalStrategy } from '@modules/auth/strategies/local.strategy';
+import { AuthController } from '@auth/auth.controller';
+import { AuthService } from '@auth/auth.service';
+import { SessionSerializer } from '@auth/session.serializer';
+import { LocalStrategy } from '@auth/strategies/local.strategy';
 import { UsersModule } from '@modules/users/users.module';
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
@@ -8,6 +9,6 @@ import { PassportModule } from '@nestjs/passport';
 @Module({
   imports: [UsersModule, PassportModule.register({ session: true })],
   controllers: [AuthController],
-  providers: [LocalStrategy, SessionSerializer],
+  providers: [LocalStrategy, SessionSerializer, AuthService],
 })
 export class AuthModule {}
