8aeade4238cdc478ee81a1a5a9d0b06cdd0ba5b2
Clément <clementfiesta@gmail.com>
2025-12-20 09:45:04 +0100

    refactor: Update PostsModule and UsersModule to use forwardRef for circular dependency resolution

--- src/modules/posts/posts.module.ts
+++ src/modules/posts/posts.module.ts
@@ -1,5 +1,5 @@
 import { DrizzleModule } from '@drizzle/drizzle.module';
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UsersModule } from '@users/users.module';
 import { PostsController } from './posts.controller';
 import { PostsRepository } from './posts.repository';
@@ -9,6 +9,6 @@ import { PostsService } from './posts.service';
   controllers: [PostsController],
   providers: [PostsRepository, PostsService],
   exports: [PostsRepository, PostsService],
-  imports: [DrizzleModule, UsersModule],
+  imports: [DrizzleModule, forwardRef(() => UsersModule)],
 })
 export class PostsModule {}
