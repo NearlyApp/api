0ae732194adaf8507fefc2b4f81d18b0eaaf8549
vo-olivier250405 <oliviervo.pro@gmail.com>
2026-01-12 20:44:48 +0100

    [ADD] => add logs

--- src/modules/posts/posts.service.ts
+++ src/modules/posts/posts.service.ts
@@ -241,6 +241,7 @@ export class PostsService {
     const mockPosts = await this.postsRepository.findMany({
       status: 'PROCESSED',
     });
+    console.debug('Mock recommended posts: ', mockPosts);
     return mockPosts;
 
     const startRecommendationFetch = Date.now();
